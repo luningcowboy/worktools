42a57669d5d6a90d5b451489cb1e2f3ef659309a
luningcowboy <luningcowboy@gmail.com>
2020-04-24 09:16:19 +0800

    new tool

--- scale_img/main.py
+++ scale_img/main.py
@@ -1,5 +1,15 @@
+import os, sys, subprocess
+from PIL import Image
 
 def run():
     print('run')
+    cmd = 'find ./emoji -name "*.png"'
+    ret = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    out = ret.stdout.readlines()
+    for fileName in out:
+        fileName = fileName.split('\n')[0]
+        print(fileName)
+        img = Image.open(fileName)
+        img.resize((135, 135), Image.ANTIALIAS).save(fileName)
 
 run()
